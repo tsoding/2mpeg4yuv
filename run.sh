710f56e6b5d6c975e13132863442fe12aa1a9d18
rexim <reximkut@gmail.com>
2021-08-21 20:22:24 +0700

    Rename main -> main_video

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-rustc -C opt-level=3 main.rs
-time ./main
+rustc -C opt-level=3 main_video.rs
+time ./main_video
 ffmpeg -y -i output.y4m -f f32le -ar 48.0k -ac 1 -i output.pcm -map 0:v:0 -map 1:a:0 output.mp4
-mplayer output.mp4
+#mplayer output.mp4
